b3de8e1650785fbb3b448a4260db0b83a9e02b3d
Thaigithub <1tranquocthai@gmail.com>
2023-11-28 10:57:08 +0700

    remove admin module from auth

--- src/infrastructure/ioc-containers/auth.module.ts
+++ src/infrastructure/ioc-containers/auth.module.ts
@@ -7,7 +7,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JWT_SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRE_HOURS } from 'app.config';
 import { AccountModule } from './account.module';
-import { AdminModule } from './admin.module';
 import { JwtStrategy } from 'infrastructure/passport/strategies/jwt.strategy';
 import { OtpProviderModule } from './otp-provider.module';
 import { OtpProviderRepositoryImpl } from 'infrastructure/repositories/otp-provider.repository.impl';
@@ -26,7 +25,6 @@ import { OtpService } from 'infrastructure/services/sms.service';
     }),
     PrismaModule,
     AccountModule,
-    AdminModule,
     OtpProviderModule,
     OtpModule,
   ],
@@ -36,6 +34,7 @@ import { OtpService } from 'infrastructure/services/sms.service';
       provide: AuthUseCase,
       useClass: AuthUseCaseImpl,
     },
+    
     {
       provide: OtpProviderRepository,
       useClass: OtpProviderRepositoryImpl,
