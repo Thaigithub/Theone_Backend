b30e836bf32bf696b44042e932be6df70079fe98
Tei <1tranquocthai@gmail.com>
2023-12-13 07:04:19 +0000

    hotfix/to-xxx-admin-company-management

--- src/domain/company/admin/response/company-admin-get-list.response.ts
+++ src/domain/company/admin/response/company-admin-get-list.response.ts
@@ -3,16 +3,20 @@ import { Account, Company, CompanyType } from '@prisma/client';
 import { PaginationResponse } from 'utils/generics/pageInfo.response';
 
 export class CompanyResponse {
-    @ApiProperty({ type: 'string' })
+    @ApiProperty({ type: Number })
+    id: Company['id'];
+    @ApiProperty({ type: String })
     name: Company['name'];
-    @ApiProperty({ type: 'string' })
+    @ApiProperty({ type: String })
     username: Account['username'];
     @ApiProperty({ type: 'enum', enum: CompanyType })
     type: Company['type'];
-    @ApiProperty({ type: 'string' })
+    @ApiProperty({ type: String })
     contactName: Company['contactName'];
-    @ApiProperty({ type: 'string' })
+    @ApiProperty({ type: String })
     contactPhone: Company['contactPhone'];
+    @ApiProperty({ type: Date })
+    regDate: Account['createdAt'];
 }
 
 export class AdminCompanyGetListResponse extends PaginationResponse<CompanyResponse> {}
