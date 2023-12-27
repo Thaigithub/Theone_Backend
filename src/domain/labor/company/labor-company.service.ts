4704b8ba9b22bf7f495ece4eb2f6013a05963310
Tei <1tranquocthai@gmail.com>
2023-12-27 04:24:27 +0000

    feature/to-487-be-company-workmngt-list&paging

--- src/domain/labor/company/labor-company.service.ts
+++ src/domain/labor/company/labor-company.service.ts
@@ -1,7 +1,89 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'services/prisma/prisma.service';
+import { PageInfo, PaginationResponse } from 'utils/generics/pagination.response';
+import { QueryPagingHelper } from 'utils/pagination-query';
+import { LaborType } from './enum/labor-company-labor-type.enum';
+import { LaborCompanyGetListRequest } from './request/labor-company-get-list.request';
+import { LaborCompanyGetListResponse } from './response/labor-company-get-list.response';
 
 @Injectable()
 export class LaborCompanyService {
     constructor(private prismaService: PrismaService) {}
+    async getList(accountId: number, query: LaborCompanyGetListRequest): Promise<LaborCompanyGetListResponse> {
+        const request = {
+            where: {
+                NOT: {
+                    application: {
+                        member: query.type && (query.type === LaborType.INDIVIDUAL ? null : undefined),
+                        team: query.type && (query.type === LaborType.TEAM ? null : undefined),
+                    },
+                },
+                application: {
+                    post: {
+                        company: {
+                            accountId,
+                        },
+                        site: {
+                            name: query.keyword && { contains: query.keyword },
+                        },
+                    },
+                },
+                startDate: {
+                    gte: query.startDate && new Date(query.startDate),
+                },
+                endDate: {
+                    lte: query.startDate && new Date(query.endDate),
+                },
+            },
+            select: {
+                id: true,
+                labor: {
+                    select: {
+                        id: true,
+                    },
+                },
+                application: {
+                    select: {
+                        team: {
+                            select: {
+                                name: true,
+                            },
+                        },
+                        member: {
+                            select: {
+                                name: true,
+                            },
+                        },
+                        post: {
+                            select: {
+                                site: {
+                                    select: {
+                                        name: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                startDate: true,
+                endDate: true,
+            },
+            ...QueryPagingHelper.queryPaging(query),
+        };
+        const labor = (await this.prismaService.contract.findMany(request)).map((item) => {
+            return {
+                contractId: item.id,
+                laborId: item.labor ? item.labor.id : null,
+                type: item.application.member ? LaborType.INDIVIDUAL : LaborType.TEAM,
+                name: item.application.member ? item.application.member.name : item.application.team.name,
+                siteName: item.application.post.site.name,
+                startDate: item.startDate,
+                endDate: item.endDate,
+            };
+        });
+        const total = await this.prismaService.contract.count({
+            where: request.where,
+        });
+        return new PaginationResponse(labor, new PageInfo(total));
+    }
 }
