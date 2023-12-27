2a930a4ac1ccbe8f3fbba7f8c90a45ad9d3f37cf
Henry <work.nhn1909@gmail.com>
2023-12-27 10:24:39 +0000

    feature/to-582-583-584-585-be-member-myhome-mycareer-general-list-create-detail-update

--- src/domain/career/member/career-member.service.ts
+++ src/domain/career/member/career-member.service.ts
@@ -1,26 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { CareerType, CodeType } from '@prisma/client';
 import { PrismaService } from 'services/prisma/prisma.service';
 import { CareerMemberCreateRequest } from './request/career-member-create.request';
-import { CareerMemberGetListRequest } from './request/career-member-get-list-request';
+import { CareerMemberGetListRequest } from './request/career-member-get-list.request';
 import { CareerResponse } from './response/career-member-get-list.response';
 import { QueryPagingHelper } from 'utils/pagination-query';
+import { CareerMemberUpdateRequest } from './request/career-member-update.request';
+import { getTimeDifferenceInMonths, getTimeDifferenceInYears } from '../../../utils/time-calculator';
 
 @Injectable()
 export class CareerMemberService {
     constructor(private readonly prismaService: PrismaService) {}
-
-    private async getMemberId(accountId: number): Promise<number> {
-        const member = await this.prismaService.member.findUnique({
-            select: {
-                id: true,
-            },
-            where: {
-                accountId,
-            },
-        });
-        return member.id;
-    }
 
     private parseConditionsFromQuery(query: CareerMemberGetListRequest, memberId: number) {
         return {
@@ -32,7 +22,7 @@ export class CareerMemberService {
 
     async getList(query: CareerMemberGetListRequest, accountId: number): Promise<CareerResponse[]> {
         const memberId = await this.getMemberId(accountId);
-        return await this.prismaService.career.findMany({
+        return this.prismaService.career.findMany({
             select: {
                 id: true,
                 type: true,
@@ -44,9 +34,35 @@ export class CareerMemberService {
                 isExperienced: true,
             },
             where: this.parseConditionsFromQuery(query, memberId),
-
+            orderBy: {
+                startDate: 'desc',
+            },
             ...QueryPagingHelper.queryPaging(query),
         });
+    }
+
+    async getDetail(id: number, accountId: number): Promise<CareerResponse> {
+        const memberId = await this.getMemberId(accountId);
+        const response = await this.prismaService.career.findUnique({
+            select: {
+                id: true,
+                type: true,
+                companyName: true,
+                siteName: true,
+                startDate: true,
+                endDate: true,
+                occupation: true,
+                isExperienced: true,
+            },
+            where: {
+                isActive: true,
+                id,
+                memberId,
+            },
+        });
+
+        if (!response) throw new NotFoundException('Career does not exist');
+        return response;
     }
 
     async getTotal(query: CareerMemberGetListRequest, accountId: number): Promise<number> {
@@ -63,6 +79,7 @@ export class CareerMemberService {
             where: {
                 id: body.occupationId,
                 codeType: CodeType.JOB,
+                isActive: true,
             },
         });
 
@@ -70,8 +87,43 @@ export class CareerMemberService {
             data: {
                 ...body,
                 memberId,
+                experiencedYears: getTimeDifferenceInYears(new Date(body.startDate), new Date(body.endDate)),
+                experiencedMonths: getTimeDifferenceInMonths(new Date(body.startDate), new Date(body.endDate)),
                 type: CareerType.GENERAL,
                 occupationId: occupation.id,
+            },
+        });
+    }
+
+    async updateCareer(id: number, body: CareerMemberUpdateRequest, accountId: number): Promise<void> {
+        const memberId = await this.getMemberId(accountId);
+        const career = await this.prismaService.career.findUnique({
+            where: {
+                id,
+                isActive: true,
+                memberId,
+            },
+        });
+
+        if (!career) throw new NotFoundException('Career does not exist');
+
+        const occupation = await this.prismaService.code.findUnique({
+            where: {
+                id: body.occupationId,
+                codeType: CodeType.JOB,
+                isActive: true,
+            },
+        });
+
+        await this.prismaService.career.update({
+            data: {
+                ...body,
+                experiencedYears: getTimeDifferenceInYears(new Date(body.startDate), new Date(body.endDate)),
+                experiencedMonths: getTimeDifferenceInMonths(new Date(body.startDate), new Date(body.endDate)),
+                occupationId: occupation.id,
+            },
+            where: {
+                id,
             },
         });
     }
@@ -86,7 +138,7 @@ export class CareerMemberService {
                 memberId,
             },
         });
-        if (!careerExist) throw new NotFoundException('Evaluation does not exist');
+        if (!careerExist) throw new NotFoundException('Career does not exist');
 
         await this.prismaService.career.update({
             where: {
@@ -98,5 +150,17 @@ export class CareerMemberService {
                 isActive: false,
             },
         });
+    }
+
+    private async getMemberId(accountId: number): Promise<number> {
+        const member = await this.prismaService.member.findUnique({
+            select: {
+                id: true,
+            },
+            where: {
+                accountId,
+            },
+        });
+        return member.id;
     }
 }
