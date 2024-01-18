d89100d1c784918c5f0fc4cdd824968c21362e63
Tei <1tranquocthai@gmail.com>
2024-01-18 04:01:25 +0000

    hotfix/to-xxx-product-payment

--- src/domain/product/webhook/product-webhook.service.ts
+++ src/domain/product/webhook/product-webhook.service.ts
@@ -5,7 +5,7 @@ import { PortoneService } from 'services/portone/portone.service';
 export class ProductWebhookService {
     constructor(private portoneService: PortoneService) {}
     async verifyPayment(body: any): Promise<void> {
-        const { imp_uid, merchant_uid } = body;
-        await this.portoneService.verifyPayment(imp_uid, merchant_uid);
+        const { imp_uid } = body;
+        await this.portoneService.verifyPayment(imp_uid);
     }
 }
