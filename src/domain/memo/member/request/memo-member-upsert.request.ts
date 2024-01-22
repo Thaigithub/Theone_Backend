c0f793e11b85833f0eddfe9952526db6175377ab
Tei <1tranquocthai@gmail.com>
2024-01-22 18:05:52 +0000

    feature/to-624-be-member-myhome-calendar-view

---
+++ src/domain/memo/member/request/memo-member-upsert.request.ts
@@ -0,0 +1,21 @@
+import { Expose } from 'class-transformer';
+import { IsOptional, IsString, Matches } from 'class-validator';
+
+export class MemoMemberUpsertRequest {
+    @Expose()
+    @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+        message: 'Date must be in the format yyyy-mm.',
+    })
+    startDate: string;
+
+    @Expose()
+    @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+        message: 'Date must be in the format yyyy-mm.',
+    })
+    endDate: string;
+
+    @Expose()
+    @IsString()
+    @IsOptional()
+    note: string;
+}
