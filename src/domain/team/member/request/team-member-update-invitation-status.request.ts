0e9dc860f5c6abf6020e45ef69976158e2f9d46d
Tei <1tranquocthai@gmail.com>
2024-01-23 07:40:04 +0000

    hotfix/to-xxx-team-ber

--- src/domain/team/member/request/team-member-update-invitation-status.request.ts
+++ src/domain/team/member/request/team-member-update-invitation-status.request.ts
@@ -1,5 +1,7 @@
 import { InvitationStatus } from '@prisma/client';
+import { Expose } from 'class-transformer';
 
 export class TeamMemberUpdateInvitationStatus {
+    @Expose()
     status: InvitationStatus;
 }
