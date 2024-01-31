25c3e399a1adb7890ec001df3334fbf01b96b62e
Tei <1tranquocthai@gmail.com>
2024-01-31 06:28:24 +0000

    hotfix/to-xxx-code-management

--- src/domain/code/guest/code-guest.controller.ts
+++ src/domain/code/guest/code-guest.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BaseResponse } from 'utils/generics/base.response';
 import { CodeAdminGetListRequest } from '../admin/request/code-admin-get-list.request';
-import { CodeAdminGetListResponse } from '../admin/response/code-admin-get-list.response';
 import { CodeMemberService } from '../member/code-member.service';
+import { CodeMemberGetListResponse } from '../member/response/code-member-get-list.response';
 
 @Controller('/guest/codes')
 export class CodeGuestController {
     constructor(private codeMemberService: CodeMemberService) {}
 
     @Get()
-    async getList(@Query() query: CodeAdminGetListRequest): Promise<BaseResponse<CodeAdminGetListResponse>> {
+    async getList(@Query() query: CodeAdminGetListRequest): Promise<BaseResponse<CodeMemberGetListResponse>> {
         return BaseResponse.of(await this.codeMemberService.getList(query));
     }
 }
